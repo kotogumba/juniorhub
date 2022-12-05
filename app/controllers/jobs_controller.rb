8865bdcf82dc1f162a470893d3761767087d5717
Scarlett Ah-Thon <scarlettahthon@gmail.com>
2022-12-05 14:18:16 +0100

    show_job

--- app/controllers/jobs_controller.rb
+++ app/controllers/jobs_controller.rb
@@ -1,6 +1,12 @@
 class JobsController < ApplicationController
+  before_action :set_job, only: [:show]
   validates :title, :description, :location, presence: true
   def show
+  end
+
+  private
+
+  def set_job
     @job = Job.find(params[:id])
   end
 end
