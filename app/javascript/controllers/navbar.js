c4046a8e67998c0f0352ae6cbc58af43e1e04df8
Lasse Sailer <l.sailer@online.de>
2022-12-05 17:15:57 +0100

    added first navbar draft

---
+++ app/javascript/controllers/navbar.js
@@ -0,0 +1,33 @@
+import { Controller } from "@hotwired/stimulus"
+
+export default class extends Controller {
+  connect() {
+    console.log('hello')
+    // this.element.textContent = "Hello World!"
+  }
+}
+
+
+// const navSlide = () => {
+//   const burger = document.querySelector('.burger');
+//   const nav = document.querySelector('.nav-links');
+//   const navLinks = document.querySelectorAll('.nav-links li');
+
+//   // toggle navbar for mobile view
+//   burger.addEventListener('click', ()=>{
+//     nav.classList.toggle('nav-active');
+
+//     // Animate links
+//     navLinks.forEach((link, index) => {
+//       if (link.style.animation) {
+//         link.style.animation = '';
+//       } else {
+//         link.style.animation = `navLinkFade 0.5s ease forwards ${index / 7 + 1.5}s`;
+//       }
+//     });
+//     // burger menu animation
+//     burger.classList.toggle('toggle');
+//   });
+// }
+
+// navSlide();
