2600314fb45082533176f84f376c3635e0a2894f
Scarlett Ah-Thon <scarlettahthon@gmail.com>
2022-12-05 14:15:18 +0100

    show_job

--- app/controllers/jobs_controller.rb
+++ app/controllers/jobs_controller.rb
@@ -1,4 +1,6 @@
 class JobsController < ApplicationController
   validates :title, :description, :location, presence: true
-  
+  def show
+    @job = Job.find(params[:id])
+  end
 end
--- app/models/job.rb
+++ app/models/job.rb
@@ -1,4 +1,6 @@
 class Job < ApplicationRecord
   belongs_to :user
   has_many :job_responses, dependent: :destroy
+  validates :title, presence: true
+  validates :content, presence: true
 end
