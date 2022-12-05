a5d392964b453926eaf2ae3abf7810e35befffba
Dmytro Plechun <ifunnd@gmail.com>
2022-12-05 13:55:43 +0200

    pundit

--- app/controllers/jobs_controller.rb
+++ app/controllers/jobs_controller.rb
@@ -1,4 +1,52 @@
 class JobsController < ApplicationController
   validates :title, :description, :location, presence: true
+
+  def index
+    @jobs = policy_scope(Job)
+  end
   
+  def show
+    authorize @job
+  end
+
+  def new
+    @job = Job.new
+    authorize @job
+  end
+
+  def create
+    @job = Job.new(job_params)
+    @job.user = current_user
+    authorize @job
+    if @job.save
+      redirect_to job_path(@job)
+    else
+      render :new
+    end
+  end
+
+  def edit
+    authorize @job
+  end
+
+  def update
+    authorize @job
+    if @job.update(job_params)
+      redirect_to job_path(@job)
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    authorize @job
+    @job.destroy
+    redirect_to jobs_path
+  end
+
+  private
+
+  def job_params
+    params.require(:job).permit(:title, :location, :content)
+  end
 end
