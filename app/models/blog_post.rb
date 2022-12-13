dfb202ee7a619e440dfb2dc4cac9992edd44d259
Dmytro Plechun <ifunnd@gmail.com>
2022-12-13 13:13:37 +0200

    images

--- app/models/blog_post.rb
+++ app/models/blog_post.rb
@@ -4,6 +4,7 @@ class BlogPost < ApplicationRecord
   belongs_to :blog
   has_many :comments, dependent: :destroy
   has_many :votes, dependent: :destroy
+  has_one_attached :image
 
   def categories
     blog = self.blog
