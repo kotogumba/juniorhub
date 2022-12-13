86cf39e512d8dda5b8ffe66ddf40b8a5176c7d10
Scarlett Ah-Thon <scarlettahthon@gmail.com>
2022-12-13 16:21:13 +0100

    bookmark feature show page

--- app/javascript/controllers/favorite_controller.js
+++ app/javascript/controllers/favorite_controller.js
@@ -2,9 +2,15 @@ import { Controller } from "@hotwired/stimulus"
 
 // Connects to data-controller="favorite"
 export default class extends Controller {
-  static targets = ["bookmark"]
+  static targets = ["bookmark", "text"]
 
   toggleFavorite () {
     this.bookmarkTarget.classList.toggle('red')
+
+    if (this.textTarget.innerHTML === "Add to your favorites") {
+      this.textTarget.innerHTML = "Added";
+    } else {
+      this.textTarget.innerHTML = "Add to your favorites"
+    }
   }
 }
